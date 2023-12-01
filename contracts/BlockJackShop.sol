ea85d71b195d099b1457aff3d1986a31fc54828d
kkennyllow <99718568+kkennyllow@users.noreply.github.com>
2023-12-02 03:51:44 +0800

    Updated BlockJackShop.sol with item and tokens

--- contracts/BlockJackShop.sol
+++ contracts/BlockJackShop.sol
@@ -1,19 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./BlockJackToken.sol";
+import "./BlockJackItem.sol";
 
-contract BlockJackNFT is ERC721URIStorage, Ownable {
-    constructor() Ownable (msg.sender) ERC721("BlockJackNFT", "BJN") {
+
+contract BlockjackShop {
+    BlockJackItem public blockJackItemContract;
+    BlockJackToken public blockJackTokenContract;
+
+    mapping(uint256 => uint256) public itemPrices;  // itemId => price
+    mapping(uint256 => address) public itemOwners;  // itemId => owner
+
+    constructor(address _blockJackItemAddress, address _blockJackTokenAddress) {
+        blockJackItemContract = BlockJackItem(_blockJackItemAddress);
+        blockJackTokenContract = BlockJackToken(_blockJackTokenAddress);
     }
 
-    function mint(
-        address _to,
-        uint256 _tokenId,
-        string calldata _uri
-    ) external onlyOwner {
-        _mint(_to, _tokenId);
-        _setTokenURI(_tokenId, _uri);
+    function listItem(uint256 itemId, uint256 price) public {
+        require(msg.sender == blockJackItemContract.ownerOf(itemId), "Not the item owner");
+        itemPrices[itemId] = price;
+        itemOwners[itemId] = msg.sender;
+        // Emit event for item listing
     }
+
+    function buyItem(uint256 itemId) public {
+        uint256 price = itemPrices[itemId];
+        require(price > 0, "Item not for sale");
+        require(blockJackTokenContract.checkCredit(msg.sender) >= price, "Insufficient balance");
+
+        blockJackTokenContract.transferCredit(itemOwners[itemId], price);
+        blockJackItemContract.safeTransferFrom(itemOwners[itemId], msg.sender, itemId);
+
+        itemPrices[itemId] = 0;  // Item no longer for sale
+        itemOwners[itemId] = address(0);
+        // Emit event for item purchase
+    }
+
+    // Additional functions like getItemPrice, removeListing, etc.
 }
