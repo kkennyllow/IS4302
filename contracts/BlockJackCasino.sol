21b4c9bc7d1154983f323a2b5dfc3cef89016615
NWH21 <99738981+NWH21@users.noreply.github.com>
2023-12-06 22:12:18 +0800

    Update BlockJackCasino.sol

--- contracts/BlockJackCasino.sol
+++ contracts/BlockJackCasino.sol
@@ -274,5 +274,6 @@ contract BlockJackCasino is Ownable {
         deckContract.clearHand(dealerAddress);
         gamblingTable.players = new address[](0);
         deckContract.beforeStand(dealerAddress);
+        deckContract.refreshDeck();
     }
 }
